155fd23fd99403b886936f4b499aa12ec570b552
Glitch (faye-arranz-main) <none>
2022-03-25 02:17:36 +0000

    🆕🏭 Updated with Glitch

--- script.js
+++ script.js
@@ -1,97 +1,103 @@
-$(document).ready(function () {
-  $("#classmate").click(function () {
-    $("#name").toggle();
-  });
+// $(document).ready(function () {
+//   $("#classmate").click(function () {
+//     $("#name").toggle();
+//   });
 
-  $("#classmate").click(function () {
-    $("#1").toggle();
-  });
-  $("#classmate").click(function () {
-    $("#2").toggle();
-  });
-  $("#classmate").click(function () {
-    $("#3").toggle();
-  });
-  $("#classmate").click(function () {
-    $("#4").toggle();
-  });
-  $("#classmate").click(function () {
-    $("#5").toggle();
-  });
-  $("#classmate").click(function () {
-    $("#6").toggle();
-  });
-  $("#classmate").click(function () {
-    $("#7").toggle();
-  });
-  $("#classmate").click(function () {
-    $("#8").toggle();
-  });
-  $("#classmate").click(function () {
-    $("#9").toggle();
-  });
-  $("#classmate").click(function () {
-    $("#10").toggle();
-  });
-  $("#classmate").click(function () {
-    $("#11").toggle();
-  });
-  $("#classmate").click(function () {
-    $("#12").toggle();
-  });
-  $("#classmate").click(function () {
-    $("#13").toggle();
-  });
-  $("#classmate").click(function () {
-    $("#14").toggle();
-  });
+//   $("#classmate").click(function () {
+//     $("#1").toggle();
+//   });
+//   $("#classmate").click(function () {
+//     $("#2").toggle();
+//   });
+//   $("#classmate").click(function () {
+//     $("#3").toggle();
+//   });
+//   $("#classmate").click(function () {
+//     $("#4").toggle();
+//   });
+//   $("#classmate").click(function () {
+//     $("#5").toggle();
+//   });
+//   $("#classmate").click(function () {
+//     $("#6").toggle();
+//   });
+//   $("#classmate").click(function () {
+//     $("#7").toggle();
+//   });
+//   $("#classmate").click(function () {
+//     $("#8").toggle();
+//   });
+//   $("#classmate").click(function () {
+//     $("#9").toggle();
+//   });
+//   $("#classmate").click(function () {
+//     $("#10").toggle();
+//   });
+//   $("#classmate").click(function () {
+//     $("#11").toggle();
+//   });
+//   $("#classmate").click(function () {
+//     $("#12").toggle();
+//   });
+//   $("#classmate").click(function () {
+//     $("#13").toggle();
+//   });
+//   $("#classmate").click(function () {
+//     $("#14").toggle();
+//   });
 
-  // STUDIO
-  $("#studio").click(function () {
-    $("#name").toggle();
-  });
-  $("#studio").click(function () {
-    $("#1.1").toggle();
-  });
-  $("#studio").click(function () {
-    $("#1.2").toggle();
-  });
-  $("#studio").click(function () {
-    $("#1.3").toggle();
-  });
-  $("#studio").click(function () {
-    $("#1.4").toggle();
-  });
+//   // STUDIO
+//   $("#studio").click(function () {
+//     $("#name").toggle();
+//   });
+//   $("#studio").click(function () {
+//     $("#1.1").toggle();
+//   });
+//   $("#studio").click(function () {
+//     $("#1.2").toggle();
+//   });
+//   $("#studio").click(function () {
+//     $("#1.3").toggle();
+//   });
+//   $("#studio").click(function () {
+//     $("#1.4").toggle();
+//   });
 
-  // LAB
-  $("#lab").click(function () {
-    $("#name").toggle();
-  });
-  $("#lab").click(function () {
-    $("#2.1").toggle();
-  });
-  $("#lab").click(function () {
-    $("#2.2").toggle();
-  });
-  $("#lab").click(function () {
-    $("#2.3").toggle();
-  });
-  $("#lab").click(function () {
-    $("#2.4").toggle();
-  });
+//   // LAB
+//   $("#lab").click(function () {
+//     $("#name").toggle();
+//   });
+//   $("#lab").click(function () {
+//     $("#2.1").toggle();
+//   });
+//   $("#lab").click(function () {
+//     $("#2.2").toggle();
+//   });
+//   $("#lab").click(function () {
+//     $("#2.3").toggle();
+//   });
+//   $("#lab").click(function () {
+//     $("#2.4").toggle();
+//   });
 
 
-  // ABOUT
-  $("#about").click(function () {
-    $("#name").toggle();
-  });
-  $("#about").click(function () {
-    $("#bio").toggle();
-  });
+//   // ABOUT
+//   $("#about").click(function () {
+//     $("#name").toggle();
+//   });
+//   $("#about").click(function () {
+//     $("#bio").toggle();
+//   });
 
-});
+// });
 
 // function showhide() {
 //   var div = document.getElementById("classmate");
-//   div.classList.toggle('name');
+//   // div.classList.toggle('name');
+//   if(div.css('visibility') == 'hidden'){
+//     div.css('visibility','visible');
+//   }
+//   else{
+//     div.css('visibility','hidden');
+//   }
 // }
